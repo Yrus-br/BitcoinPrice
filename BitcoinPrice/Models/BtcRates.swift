1cd150810de232358f380befb0902067445ec725
Yurii <yourussianbrother@gmail.com>
2022-11-10 01:25:30 +0300

    fixing the NetworkModel

--- BitcoinPrice/Models/BtcRates.swift
+++ BitcoinPrice/Models/BtcRates.swift
@@ -6,28 +6,12 @@
 //
 
 struct BtcRates: Codable {
-    let rates: Currency
+    let rates: [String: Rate]
 }
 
-struct Currency: Codable {
-    let btc:  Price
-    let eth: Price
-    let ltc: Price
-    let bch: Price
-    let bnb: Price
-    let eos: Price
-    let xrp: Price
-    let xlm: Price
-    let link: Price
-    let dot: Price
-    let yfi: Price
-    let usd: Price
-}
-
-struct Price: Codable {
+struct Rate: Codable {
     let name: String
-    let unit: String
+let unit: String
     let value: Double
     let type: String
 }
-
